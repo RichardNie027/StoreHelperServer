74402e19a8d8bc0d1740326aa55184ef38873537
RichardNie <54984573@qq.com>
2019-08-01 18:14:49 +0800

    daily commit

--- src/main/java/com/tlg/storehelper/StorehelperApplication.java
+++ src/main/java/com/tlg/storehelper/StorehelperApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * SpringBoot默认扫描Application入口类所在的包以及子包，也可以主动配置
@@ -14,6 +15,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 //@EnableAutoConfiguration    //自动载入应用程序所需的所有Bean，在类路径中查找
 @ServletComponentScan       //注册过滤器注解
 @MapperScan(basePackages = {"com.tlg.storehelper.dao.main","com.tlg.storehelper.dao.second"})   //扫描dao
+@EnableTransactionManagement
 public class StorehelperApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/tlg/storehelper/service/impl/BusinessServiceImpl.java
+++ src/main/java/com/tlg/storehelper/service/impl/BusinessServiceImpl.java
@@ -16,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -53,6 +54,7 @@ public class BusinessServiceImpl implements BusinessService {
         return inventoryDetailMapper.selectByParentId(pid);
     }
 
+    @Transactional
     @Override
     public String uploadInventory(InventoryEntity inventoryEntity) {
         if(inventoryMapper.selectById(inventoryEntity.id) != null) {
